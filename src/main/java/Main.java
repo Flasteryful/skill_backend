5d8e627805003e9209e0fbdbec76964a12b810d0
Artem Ivanov <a.sergeevich.ivanov@mail.ru>
2022-04-16 10:57:28 +0300

    added  chapter 27.8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,30 +23,30 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-        StudentComparator studentComparator = GetComparator.getStudentComparator(StudentComparatorList.STUDENT_AVGEXAMSCORE_COMPARATOR);
-        UniversityComparator universityComparator = GetComparator.getUniversityComparator(UniversityComparatorList.UNIVERSITY_FULLNAME_COMPARATOR);
-
-        Student student0 = new Student();
-        student0.setFullName("Иванов А.С.");
-        student0.setUniversityId("657");
-        student0.setCurrentCourseNumber(4);
-        student0.setAvgExamScore(4.7f);
-        Student student1 = new Student();
-        student1.setFullName("Иванов А.С.");
-        student1.setUniversityId("657");
-        student1.setCurrentCourseNumber(5);
-        student1.setAvgExamScore(4.8f);
-        List<Student> students = new ArrayList<>();
-        students.add(student0);
-        students.add(student1);
-        students.stream().filter(student -> student.getAvgExamScore() == 4.8f)
-                .peek(student -> System.out.println("filter " + JsonUtil.StudentSerialize(student)))
-                .peek(student -> System.out.println("filter " + JsonUtil.StudentDeserialize(JsonUtil.StudentSerialize(student))))
-                .collect(Collectors.toList());
+//        StudentComparator studentComparator = GetComparator.getStudentComparator(StudentComparatorList.STUDENT_AVGEXAMSCORE_COMPARATOR);
+//        UniversityComparator universityComparator = GetComparator.getUniversityComparator(UniversityComparatorList.UNIVERSITY_FULLNAME_COMPARATOR);
+//
+//        Student student0 = new Student();
+//        student0.setFullName("Иванов А.С.");
+//        student0.setUniversityId("657");
+//        student0.setCurrentCourseNumber(4);
+//        student0.setAvgExamScore(4.7f);
+//        Student student1 = new Student();
+//        student1.setFullName("Иванов А.С.");
+//        student1.setUniversityId("657");
+//        student1.setCurrentCourseNumber(5);
+//        student1.setAvgExamScore(4.8f);
+//        List<Student> students = new ArrayList<>();
+//        students.add(student0);
+//        students.add(student1);
+//        students.stream().filter(student -> student.getAvgExamScore() == 4.8f)
+//                .peek(student -> System.out.println("filter " + JsonUtil.StudentSerialize(student)))
+//                .peek(student -> System.out.println("filter " + JsonUtil.StudentDeserialize(JsonUtil.StudentSerialize(student))))
+//                .collect(Collectors.toList());
 
 
         XlsWriter xlsWriter = new XlsWriter();
-        xlsWriter.writeStatistics(StatisticsProcessUtil.collectStatistics(Reader.readStudent(),Reader.readUniversity()),"universityInfo.xlsx");
+        xlsWriter.writeStatistics(StatisticsProcessUtil.collectStatistics(Reader.readStudent(),Reader.readUniversity()),"statistics.xlsx");
 
     }
 }
